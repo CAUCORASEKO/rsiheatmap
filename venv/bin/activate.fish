0b84ab63982e3cd91f35f8ca75f3bc7b6d317a54
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-08-10 19:56:58 +0300

    all files

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/mestizo/rsiheatmap1/rsiheatmap/venv"
+set -gx VIRTUAL_ENV "/Users/mestizo/Desktop/rsiheatmap/rsiheatmap/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
